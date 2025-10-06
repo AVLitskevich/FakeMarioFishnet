2c48068fe7a57c8e06a05a10390289871329840c
turichetoya@gmail.com <turichetoya@gmail.com>
2025-10-06 15:41:33 +0400

    Update PlayerColor.cs

--- Assets/Scripts/PlayerColor.cs
+++ Assets/Scripts/PlayerColor.cs
@@ -5,12 +5,6 @@ namespace DefaultNamespace
 {
     public class PlayerColor
     {
-        [SerializeField] private int _paletteIndex;
-        [SerializeField] private Color _newColor = new Color(0.2f, 0.6f, 1f, 1f);
-
-        private ColorSwap_HeroKnight _colorSwap;
         
-        [SyncVar(OnChange = nameof(OnColorChanged))]
-        private Color _playerColor
     }
 }
