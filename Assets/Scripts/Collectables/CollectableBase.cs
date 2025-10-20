4d5734ac173c93b7fe47a3b995750f48840d8bbc
turichetoya@gmail.com <turichetoya@gmail.com>
2025-10-20 19:49:59 +0500

    2 new files: CollectableBase and PlayerCollector

---
+++ Assets/Scripts/Collectables/CollectableBase.cs
@@ -0,0 +1,46 @@
+using FishNet.Object;
+using UnityEngine;
+
+namespace DefaultNamespace.Collectables
+{
+
+    public enum CollectableType
+    {
+        Unknown = 0
+    }
+    [RequireComponent(typeof(Collider2D))]
+    public class CollectableBase : NetworkBehaviour
+    {
+        [SerializeField] private CollectableType _type = CollectableType.Unknown;
+        public CollectableType Type => _type;
+        
+        public bool Collected { get; private set; }
+        
+        public virtual bool CanBeCollectedServer(PlayerCollector playerCollector) => !Collected && IsServerInitialized;
+
+        public bool TryCollectServer(PlayerCollector playerCollector)
+        {
+            if (!IsServerInitialized || !CanBeCollectedServer(playerCollector))
+            {
+                return false;
+            }
+
+            Collected = true;
+            return true;
+        }
+        
+        public virtual void OnCollectedServer(PlayerCollector playerCollector)
+        {
+            if (!IsServerInitialized) return;
+
+            if (NetworkObject != null && NetworkObject.IsSpawned)
+            {
+                NetworkObject.Despawn();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
