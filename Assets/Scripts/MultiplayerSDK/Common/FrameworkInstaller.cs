a8f0e21baacd60a6f10da92a542be48374a51119
Aleksandr Litskevich <avlitskevich@gmail.com>
2025-10-23 20:05:56 +0500

    Fixed layers and added little bit of smoothing

--- Assets/Scripts/MultiplayerSDK/Common/FrameworkInstaller.cs
+++ Assets/Scripts/MultiplayerSDK/Common/FrameworkInstaller.cs
@@ -1,4 +1,5 @@
 ﻿using MultiplayerSDK.DI;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -6,10 +7,13 @@ namespace MultiplayerSDK.Common
 {
     public class FrameworkInstaller : MonoInstaller
     {
+        [SerializeField] private LayerProvider _layerProvider;
+        
         public override void Install(IContainerBuilder builder)
         {
             builder.RegisterEntryPoint<ContainerSingletonWrapper>();
             builder.Register<GlobalGameData>(Lifetime.Singleton);
+            builder.RegisterInstance(_layerProvider);
         }
     }
 }
